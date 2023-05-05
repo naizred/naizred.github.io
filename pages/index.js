7642dd3043e3e19936e8f587f843b36fc7178458
Elek Balazs <elek.balazs86@gmail.com>
2023-05-05 15:19:24 +0200

    temporary removed database related functions

--- pages/index.js
+++ pages/index.js
@@ -1,9 +1,9 @@
 import Head from 'next/head';
 import styles from '../styles/Home.module.css';
 import { PrismaClient } from '@prisma/client'
+import React from "react";
 
-
-/* export const getStaticProps = async () => {
+/*  export const getStaticProps = async () => {
   const prisma = new PrismaClient()
   const feed = await prisma.ttkweapons.findMany();
 
@@ -12,7 +12,7 @@ import { PrismaClient } from '@prisma/client'
       feed:JSON.parse(JSON.stringify(feed))
     }
   };
-}; */
+};  */
 
 export default function Home(props) {
   let bodyParts = ["bal láb", "jobb láb", "bal kar", "fegyverforgató kar", "fegyverforgató kar", "törzs", "törzs", "törzs", "törzs", "fej"];
@@ -63,37 +63,33 @@ function hitChecker(lightDice) {
   damageResult.style.opacity = '0'
   bodyPart.innerText = ''
   bodyPart.style.opacity = '0'
-  /*  function flashEffect500(element){element.animate([{ color: 'white' }, { color: 'black' }], 500);}
-   function animateOpacity1500(element){element.animate([{ opacity: '0' }, { opacity: '1' }], 1500);}
-   animateOpacity1500(rollResult) */
-  rollResult.animate([{ opacity: '0' }, { opacity: '1' }], 1500);
-  // function timeoutSetter(functionToTimeout, timeOut){setTimeout(()=>{functionToTimeout},timeOut)}
 
-  // timeoutSetter(animateOpacity1500(damageResult),500)
+  rollResult.animate([{ opacity: '0' }, { opacity: '1' }], 1500);
+
   setTimeout(() => {
       damageResult.animate([{ opacity: '0' }, { opacity: '1' }], 1500);
   }, 500);
-  // timeoutSetter(animateOpacity1500(bodyPart),1000)
+
   setTimeout(() => {
       bodyPart.animate([{ opacity: '0' }, { opacity: '1' }], 1500);
   }, 1000);
-  //timeoutSetter((rollResult.style.opacity = '1'),1500)
+
   setTimeout(() => {
       rollResult.style.opacity = '1'
   }, 1500);
-  //timeoutSetter((damageResult.style.opacity = '1'),2000)
+
   setTimeout(() => {
       damageResult.style.opacity = '1'
   }, 2000);
-  //timeoutSetter((bodyPart.style.opacity = '1'),2500)
+
   setTimeout(() => {
       bodyPart.style.opacity = '1'
   }, 2500);
-  //timeoutSetter(flashEffect500(rollResult),1500)
+
   setTimeout(() => {
       rollResult.animate([{ color: 'white' }, { color: 'black' }], 500);
   }, 1500);
-  //timeoutSetter(flashEffect500(damageResult),2000)
+
   setTimeout(() => {
       damageResult.animate([{ color: 'white' }, { color: 'black' }], 500);
   }, 2000);
@@ -145,6 +141,109 @@ setTimeout(() => {
 setTimeout(() => {
     charAtkSum.animate([{ color: 'white' }, { color: 'black' }], 500);
 }, 4750);
+    damageResult.innerText = darkDice
+    bodyPart.innerText = hitChecker(lightDice)
+
+    setTimeout(() => {
+        let tempImg = document.createElement('img')
+        tempImg.classList.add('tempImg')
+        bodyPartImg.appendChild(tempImg)
+        if (bodyPart.innerText == "bal láb") {
+            tempImg.src = ''
+            tempImg.src = './bodyParts/LeftLeg.png'
+            tempImg.animate([{ opacity: '0' }, { opacity: '1' }], 600);
+            setTimeout(() => {
+                tempImg.style.opacity = '0'
+                tempImg.animate([{ opacity: '0' }, { opacity: '1' }], 600);
+            }, 600);
+            setTimeout(() => {
+                tempImg.style.opacity = '0'
+                tempImg.animate([{ opacity: '0' }, { opacity: '1' }], 600);
+                tempImg.style.opacity = '1'
+            }, 1200);
+        }
+
+        if (bodyPart.innerText == "jobb láb") {
+            tempImg.src = ''
+            tempImg.src = './bodyParts/RightLeg.png'
+            tempImg.animate([{ opacity: '0' }, { opacity: '1' }], 600);
+            setTimeout(() => {
+                tempImg.style.opacity = '0'
+                tempImg.animate([{ opacity: '0' }, { opacity: '1' }], 600);
+            }, 600);
+            setTimeout(() => {
+                tempImg.style.opacity = '0'
+                tempImg.animate([{ opacity: '0' }, { opacity: '1' }], 600);
+                tempImg.style.opacity = '1'
+            }, 1200);
+        }
+
+        if (bodyPart.innerText == "bal kar") {
+            tempImg.src = ''
+            tempImg.src = './bodyParts/LeftArm.png'
+            tempImg.animate([{ opacity: '0' }, { opacity: '1' }], 600);
+            setTimeout(() => {
+                tempImg.style.opacity = '0'
+                tempImg.animate([{ opacity: '0' }, { opacity: '1' }], 600);
+            }, 600);
+            setTimeout(() => {
+                tempImg.style.opacity = '0'
+                tempImg.animate([{ opacity: '0' }, { opacity: '1' }], 600);
+                tempImg.style.opacity = '1'
+            }, 1200);
+        }
+
+        if (bodyPart.innerText == "fegyverforgató kar") {
+            tempImg.src = ''
+            tempImg.src = './bodyParts/RightArm.png'
+            tempImg.animate([{ opacity: '0' }, { opacity: '1' }], 600);
+            setTimeout(() => {
+                tempImg.style.opacity = '0'
+                tempImg.animate([{ opacity: '0' }, { opacity: '1' }], 600);
+            }, 600);
+            setTimeout(() => {
+                tempImg.style.opacity = '0'
+                tempImg.animate([{ opacity: '0' }, { opacity: '1' }], 600);
+                tempImg.style.opacity = '1'
+            }, 1200);
+        }
+
+        if (bodyPart.innerText == "törzs") {
+            tempImg.src = ''
+            tempImg.src = './bodyParts/Torso.png'
+            tempImg.animate([{ opacity: '0' }, { opacity: '1' }], 600);
+            setTimeout(() => {
+                tempImg.style.opacity = '0'
+                tempImg.animate([{ opacity: '0' }, { opacity: '1' }], 600);
+            }, 600);
+            setTimeout(() => {
+                tempImg.style.opacity = '0'
+                tempImg.animate([{ opacity: '0' }, { opacity: '1' }], 600);
+                tempImg.style.opacity = '1'
+            }, 1200);
+        }
+
+        if (bodyPart.innerText == "fej") {
+            tempImg.src = ''
+            tempImg.src = './bodyParts/Head.png'
+            tempImg.animate([{ opacity: '0' }, { opacity: '1' }], 600);
+            setTimeout(() => {
+                tempImg.style.opacity = '0'
+                tempImg.animate([{ opacity: '0' }, { opacity: '1' }], 600);
+            }, 600);
+            setTimeout(() => {
+                tempImg.style.opacity = '0'
+                tempImg.animate([{ opacity: '0' }, { opacity: '1' }], 600);
+                tempImg.style.opacity = '1'
+            }, 1200);
+        }
+    }, 2550);
+
+
+    setTimeout(() => {
+        //bodyPart.animate([{textShadow:'0px 0px 200px black'},{textShadow:'0px 0px 1px black'}], 500);
+        bodyPart.animate([{ color: 'white' }, { color: 'black' }], 500);
+    }, 2500);
 }
   return (
     <div>
@@ -157,14 +256,15 @@ setTimeout(() => {
         
       </Head>
 
-      <main>
+      <>
+        
         <div className={styles.resultContainer}>
-        <div class="result inText">A dobás eredménye</div>
-        <div id="rollResult" class="result inNumber"></div>
-        <div class="damage inText">A sebzés</div>
-        <div id="damageResult" class="result inNumber"></div>
-        <div class="damage hitCheck">A találat helye</div>
-        <div id="bodyPart"></div>
+        <div className="result inText">A dobás eredménye</div>
+        <div id="rollResult" className="result inNumber"></div>
+        <div className="damage inText">A sebzés</div>
+        <div id="damageResult" className="result inNumber"></div>
+        <div className="damage hitCheck">A találat helye</div>
+        <div id="bodyPart" className={styles.bodyPart}></div>
     </div>
  {/*    <div className={styles.weaponsContainer}>
         <button id="addWeaponButton">Új fegyver hozzáadása</button>
@@ -179,34 +279,33 @@ setTimeout(() => {
             </select>
         </form>
         <button type="submit" name="submit" form="addWeaponForm" id="addWeaponFormSubmitButton">Elküld</button>
-        <label htmlFor="weapons" id="chosenWeapon">Választott fegyver:</label>
-          <select id="weapons" name="weapons">
-          {props.feed.map((e) => {
-                return <option id={e.w_id}>{e.w_name}</option>;
-              })}
-            <option value="">Válassz fegyvert:</option> 
-        </select>
+        
         <label htmlFor="characters" id="chosenCharacter">Választott karakter:</label>
         <select id="characters" name="characters">
           </select>
           </div> */}
+    {/*     <label  htmlFor="weapons" id="chosenWeapon">Választott fegyver:</label>
+          <select id="weapons" name="weapons">
+          {props.feed.map((e) => {
+                return <option id={e.w_id}>{e.w_name}</option>;
+              })}
+                    </select> */}
           <div id="bodyPartImg"></div>
-          <div class="welcomeUser"></div>
+          <div className="welcomeUser"></div>
 
- <div class="charStats">
-    <label for="charStr">Karakter erő</label>
+ <div className="charStats">
+    <label htmlFor="charStr">Karakter erő</label>
     <input type="text" name="charStr" id="charStr"/>
-    <label for="charAtk">Karakter TÉ</label>
+    <label htmlFor="charAtk">Karakter TÉ</label>
     <input type="text" name="charAtk" id="charAtk"/>
 </div> 
 <button id="rollButton" className={styles.rollButton} onClick={handleClick}>Dobj!</button>
 
-<div class="result inText" id="charAtkSumText">Össz TÉ</div>
-<div id="charAtkSum" class="result inNumber"></div>
+<div className="result inText" id="charAtkSumText">Össz TÉ</div>
+<div id="charAtkSum" className="result inNumber"></div>
 
-<img src="" alt="" class="gifContainer"/>
-    
-      </main>
+{/* <img src="" alt="" className="gifContainer"/> */}
+      </>
     </div>
   )
 }
