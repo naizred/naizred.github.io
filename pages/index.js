242258dc9bcd5aa8c4b6aae77381063bb72cb12a
Elek Balazs <elek.balazs86@gmail.com>
2023-07-15 13:49:35 +0200

    Added strBonus data to weapons. Now it knows if a weapon has strbonus, and only then will calculate accordingly. Weapon names sorted.

--- pages/index.js
+++ pages/index.js
@@ -2,6 +2,7 @@ import Head from "next/head";
 import styles from "../styles/Home.module.css";
 import React from "react";
 import path from "path";
+
 /* function fileReader(fileName){
   let object = fs.readFileSync(fileName)
 
@@ -13,9 +14,10 @@ console.log(fileReader("weapons.json")); */
 export const getStaticProps = async () => {
   const fs = require("fs");
   const jsonDirectory = path.join(process.cwd(), "json");
-  const feed = (JSON.parse(
-    fs.readFileSync(jsonDirectory + "/data.json")
-  ));
+  let feed = JSON.parse(
+    fs.readFileSync(jsonDirectory + "/data.json", "utf8"))
+  ;
+
 
   return {
     props: {
@@ -26,6 +28,31 @@ export const getStaticProps = async () => {
 
 export default function Home(props) {
 
+//custom sort function to sort data by name
+
+    let alphabets = ["A", "Á","B", "C", "D","E","É","F","G","H","I","J","K","L","M","N","O","Ó","Ö","Ő","P","Q","R","S",
+                "T","U","Ú","Ü","Ű","V","W","X","Z"];
+
+let aChar;
+let bChar;
+function OrderFunc(){
+   props.feed.sort(function (a, b) {
+       return CharCompare(a.w_name, b.w_name, 0);
+   });
+}
+function CharCompare(a, b, index) {
+
+  aChar = alphabets.indexOf(a.toUpperCase().charAt(index));
+  bChar = alphabets.indexOf(b.toUpperCase().charAt(index));
+  if (aChar != bChar)
+      return aChar - bChar
+  else
+      return CharCompare(a,b,index+1)
+}
+
+//custom sort function call
+OrderFunc(props.feed)
+  
   let destroyerLevel = [0, 1, 2, 3];
   let professionLevel = [0, 1, 2, 3, 4, 5];
   let bodyParts = [
@@ -44,8 +71,13 @@ export default function Home(props) {
   let darkDice = 0;
   let lightDice = 0;
 
-  function ttkRoll() {
+//------------------------------------------------------------------------
+  //-------A dobás ------
+
+  function ttkRoll(strBonus) {
+    if(strBonus==false || strBonus == true){
     let result = 0;
+
     darkDice = Math.floor(Math.random() * 10);
     lightDice = Math.floor(Math.random() * 10);
 
@@ -70,32 +102,6 @@ export default function Home(props) {
     if (lightDice == 0) {
       lightDice = 10;
     }
-    if (Math.floor(parseInt(charStr.value) / 2) > darkDice) {
-      darkDice = Math.floor(parseInt(charStr.value) / 2);
-    }
-
-    return result;
-  }
-
-  function hitChecker(lightDice) {
-    return bodyParts[lightDice - 1];
-  }
-
-  async function handleClick() {
-    bodyPartImg.innerHTML = "";
-    charAtkSum.innerText = "";
-    specialEffect.innerText = "nincs";
-    rollButton.disabled = true;
-
-    rollResult.innerText = ttkRoll();
-
-    damageResult.innerText = "";
-
-    bodyPart.innerText = "";
-
-    setTimeout(() => {
-      rollButton.disabled = false;
-    }, 350);
 
     const specialModifiers = [
       "Veszítesz 3 cselekedetet",
@@ -108,120 +114,165 @@ export default function Home(props) {
     const specialCases2 = [5, 6, 7];
     const specialCases3 = [8, 9];
 
-          if (charAtk.value == "") {
-        charAtkSum.innerText = rollResult.innerText;
-      } else {
-        charAtkSum.innerText =
-          parseFloat(rollResult.innerText) + parseFloat(charAtk.value);
-      }
+    if (lightDice == darkDice && specialCases1.includes(darkDice)) {
+      specialEffect.innerText = specialModifiers[1];
+    } else if (lightDice == darkDice && specialCases2.includes(darkDice)) {
+      specialEffect.innerText = specialModifiers[2];
+    } else if (lightDice == darkDice && specialCases3.includes(darkDice)) {
+      specialEffect.innerText = specialModifiers[3];
+    } else if (lightDice == darkDice && darkDice == 1) {
+      specialEffect.innerText = specialModifiers[0];
+    } else if (lightDice == darkDice && darkDice == 10) {
+      specialEffect.innerText = specialModifiers[4];
+    }
 
-      if (lightDice == darkDice && specialCases1.includes(darkDice)) {
-        specialEffect.innerText = specialModifiers[1];
-      } else if (lightDice == darkDice && specialCases2.includes(darkDice)) {
-        specialEffect.innerText = specialModifiers[2];
-      } else if (lightDice == darkDice && specialCases3.includes(darkDice)) {
-        specialEffect.innerText = specialModifiers[3];
-      } else if (lightDice == darkDice && darkDice == 1) {
-        specialEffect.innerText = specialModifiers[0];
-      } else if (lightDice == darkDice && darkDice == 10) {
-        specialEffect.innerText = specialModifiers[4];
-      };
+    if (strBonus == true) {
+      if (Math.floor(parseInt(charStr.value) / 2) > darkDice) {
+        darkDice = Math.floor(parseInt(charStr.value) / 2);
+      }
+    }
+       
+        return result;
+      
+      }
+    
+  }
+
+  function hitChecker(lightDice) {
+    return bodyParts[lightDice - 1];
+  }
+
+  async function handleClick() {
+    bodyPartImg.innerHTML = "";
+    charAtkSum.innerText = "";
+    specialEffect.innerText = "nincs";
+  
+    
+    const evalCurrentWeaponStrBonus = props.feed.find(
+      (name) => name.w_name === `${weapons.value}`
+    )
+    console.log(evalCurrentWeaponStrBonus.strBonusDmg)
+    
+    if (evalCurrentWeaponStrBonus.strBonusDmg == "false") {
+      rollResult.innerText = ttkRoll(false);
+    } else if (evalCurrentWeaponStrBonus.strBonusDmg == "true" && charStr.value == "") {
+      alert("Ez egy erő sebzéssel rendelkező fegyver, írd be az erődet!")
+      return
+    } else if (evalCurrentWeaponStrBonus.strBonusDmg == "true" && charStr.value != "") {
+      rollResult.innerText = ttkRoll(true);
+    }
+    
+
+
+     
+  
+    damageResult.innerText = "";
+
+    bodyPart.innerText = "";
+
+    if (charAtk.value == "") {
+      charAtkSum.innerText = rollResult.innerText;
+    } else {
+      charAtkSum.innerText =
+        parseFloat(rollResult.innerText) + parseFloat(charAtk.value);
+    }
+
+
 
     bodyPart.innerText = hitChecker(lightDice);
 
-    
-      let tempImg = document.createElement("img");
-      tempImg.classList.add("tempImg");
-      bodyPartImg.appendChild(tempImg);
-      function currentBodypart(bodypart) {
-        tempImg.src = "";
-        tempImg.src = `./bodyParts/${bodypart}`;
-        tempImg.animate([{ opacity: "0" }, { opacity: "1" }], 100);
-      }
+    let tempImg = document.createElement("img");
+    tempImg.classList.add("tempImg");
+    bodyPartImg.appendChild(tempImg);
+    function currentBodypart(bodypart) {
+      tempImg.src = "";
+      tempImg.src = `./bodyParts/${bodypart}`;
+      tempImg.animate([{ opacity: "0" }, { opacity: "1" }], 100);
+    }
 
-      if (bodyPart.innerText == "bal láb") {
-        currentBodypart("LeftLeg.png");
-      }
-      if (bodyPart.innerText == "jobb láb") {
-        currentBodypart("RightLeg.png");
-      }
+    if (bodyPart.innerText == "bal láb") {
+      currentBodypart("LeftLeg.png");
+    }
+    if (bodyPart.innerText == "jobb láb") {
+      currentBodypart("RightLeg.png");
+    }
 
-      if (bodyPart.innerText == "bal kar") {
-        currentBodypart("LeftArm.png");
-      }
+    if (bodyPart.innerText == "bal kar") {
+      currentBodypart("LeftArm.png");
+    }
 
-      if (bodyPart.innerText == "fegyverforgató kar") {
-        currentBodypart("RightArm.png");
-      }
+    if (bodyPart.innerText == "fegyverforgató kar") {
+      currentBodypart("RightArm.png");
+    }
 
-      if (bodyPart.innerText == "törzs") {
-        currentBodypart("Torso.png");
-      }
+    if (bodyPart.innerText == "törzs") {
+      currentBodypart("Torso.png");
+    }
 
-      if (bodyPart.innerText == "fej") {
-        currentBodypart("Head.png");
-      };
+    if (bodyPart.innerText == "fej") {
+      currentBodypart("Head.png");
+    }
 
-    const result = props.feed.find(
+    const damageOfCurrentWeapon = props.feed.find(
       (name) => name.w_name === `${weapons.value}`
     );
-    console.log(result.w_damage);
+    console.log(damageOfCurrentWeapon.w_damage);
 
-    if (result.w_damage === "2k10") {
+    if (damageOfCurrentWeapon.w_damage === "2k10") {
       damageResult.innerText =
         darkDice +
         lightDice +
         parseInt(destroyerLevelSelect.value) +
         parseInt(professionLevelSelect.value);
-    } else if (result.w_damage === "2k5") {
+    } else if (damageOfCurrentWeapon.w_damage === "2k5") {
       damageResult.innerText =
         Math.ceil(darkDice / 2) +
         Math.ceil(lightDice / 2) +
         parseInt(destroyerLevelSelect.value) +
         parseInt(professionLevelSelect.value);
-    } else if (result.w_damage === "2k5+1") {
+    } else if (damageOfCurrentWeapon.w_damage === "2k5+1") {
       damageResult.innerText =
         Math.ceil(darkDice / 2) +
         Math.ceil(lightDice / 2) +
         parseInt(destroyerLevelSelect.value) +
         parseInt(professionLevelSelect.value) +
         1;
-    } else if (result.w_damage === "2k5+2") {
+    } else if (damageOfCurrentWeapon.w_damage === "2k5+2") {
       damageResult.innerText =
         Math.ceil(darkDice / 2) +
         Math.ceil(lightDice / 2) +
         parseInt(destroyerLevelSelect.value) +
         parseInt(professionLevelSelect.value) +
         2;
-    } else if (result.w_damage === "1k5") {
+    } else if (damageOfCurrentWeapon.w_damage === "1k5") {
       damageResult.innerText =
         Math.ceil(darkDice / 2) +
         parseInt(destroyerLevelSelect.value) +
         parseInt(professionLevelSelect.value);
-    } else if (result.w_damage === "1k5+1") {
+    } else if (damageOfCurrentWeapon.w_damage === "1k5+1") {
       damageResult.innerText =
         Math.ceil(darkDice / 2) +
         parseInt(destroyerLevelSelect.value) +
         parseInt(professionLevelSelect.value) +
         1;
-    } else if (result.w_damage === "1k5+2") {
+    } else if (damageOfCurrentWeapon.w_damage === "1k5+2") {
       damageResult.innerText =
         Math.ceil(darkDice / 2) +
         parseInt(destroyerLevelSelect.value) +
         parseInt(professionLevelSelect.value) +
         2;
-    } else if (result.w_damage === "3k5") {
+    } else if (damageOfCurrentWeapon.w_damage === "3k5") {
       damageResult.innerText =
         Math.ceil(darkDice / 2) * 2 +
         Math.ceil(lightDice / 2) +
         parseInt(destroyerLevelSelect.value) +
         parseInt(professionLevelSelect.value);
-    } else if (result.w_damage === "1k10") {
+    } else if (damageOfCurrentWeapon.w_damage === "1k10") {
       damageResult.innerText =
         darkDice +
         parseInt(destroyerLevelSelect.value) +
         parseInt(professionLevelSelect.value);
-    } else if (result.w_damage === "1k2") {
+    } else if (damageOfCurrentWeapon.w_damage === "1k2") {
       if (darkDice > 5) {
         darkDice = 2;
       } else {
@@ -231,7 +282,7 @@ export default function Home(props) {
         darkDice +
         parseInt(destroyerLevelSelect.value) +
         parseInt(professionLevelSelect.value);
-    } else if (result.w_damage === "2k2") {
+    } else if (damageOfCurrentWeapon.w_damage === "2k2") {
       if (darkDice > 5) {
         darkDice = 2;
       } else {
@@ -271,15 +322,15 @@ export default function Home(props) {
             Választott fegyver:
           </label>
           <select id="weapons" name="weapons">
-            {props.feed.map((e) => {
+             {props.feed.map((e) => {
               return (
-                <option key={e.w_id} id={e.w_damage}>
+                <option key={e.w_id}>
                   {e.w_name}
                 </option>
               );
-            })}
+            })} 
           </select>
-          <label htmlFor="profession" id="profession">
+          <label htmlFor="professionLevelSelect" id="profession">
             Képzettség foka:
           </label>
           <select id="professionLevelSelect" name="profession">
@@ -287,7 +338,7 @@ export default function Home(props) {
               return <option key={e}>{e}</option>;
             })}
           </select>
-          <label htmlFor="destroyer" id="destroyer">
+          <label htmlFor="destroyerLevelSelect" id="destroyer">
             Pusztító adottság:
           </label>
           <select id="destroyerLevelSelect" name="destroyer">
