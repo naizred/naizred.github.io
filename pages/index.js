3ff4f3eb5f693635247719445bd57f5a00449ae8
Elek Balazs <elek.balazs86@gmail.com>
2023-09-09 08:53:58 +0200

    tried to outsource filreader but failed

--- pages/index.js
+++ pages/index.js
@@ -46,6 +46,7 @@ export const getStaticProps = async () => {
     },
   };
 };
+
 export let rollOptions = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9];
 export let filteredArrayIfHasExtraReaction
 let filteredArrayIfHasParry
