d0e3b822b01136959b168067b98133e0e0b7e799
Elek Balazs <elek.balazs86@gmail.com>
2023-09-19 17:27:27 +0200

    corrected a quick error regarding skillcheck select default value

--- Components/SkillCheck.js
+++ Components/SkillCheck.js
@@ -234,8 +234,8 @@ function SkillCheck(props) {
         <label htmlFor="skills" id="skillsLabel" className="skillCheckLabel">
             Választott képzettség:
           </label>
-            <select id="skills" name="skills" className="skillCheckSelect" onChange={evaluateSkillOrAttributeCheckBase}>
-            <option value='' disabled selected>Válassz képzettséget</option>
+            <select defaultValue="" id="skills" name="skills" className="skillCheckSelect" onChange={evaluateSkillOrAttributeCheckBase}>
+            <option value='' disabled>Válassz képzettséget</option>
           </select>
           <label htmlFor="attributes" id="attributesLabel" className="skillCheckLabel">
             Választott tulajdonság:
