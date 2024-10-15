42fff0a0d809a84db10bff31f85845c4a656fdc7
naizred <elek.balazs86@gmail.com>
2024-10-15 20:16:20 +0200

    upraded the update API so that it would only send data that were updated

--- pages/api/updateCharacter.js
+++ pages/api/updateCharacter.js
@@ -24,14 +24,10 @@ export default async function updateCharStats(req, res) {
 
   const dataToUpdate = {}
 
-  if (currentFp && parseInt(currentStats.currentFp) !== parseInt(currentFp)) {
-    dataToUpdate.currentFp = currentFp
-  } else {
-    dataToUpdate.currentFp = undefined
-  }
-  if (currentEp && currentStats.currentEp !== currentEp) dataToUpdate.currentEp = currentEp;
-  if (currentPp && currentStats.currentPp !== currentPp) dataToUpdate.currentPp = currentPp;
-  if (currentMp && currentStats.currentMp !== currentMp) dataToUpdate.currentMp = currentMp;
+  if (parseInt(currentStats.currentFp) !== parseInt(currentFp)) dataToUpdate.currentFp = currentFp;
+  if (currentStats.currentEp !== currentEp) dataToUpdate.currentEp = currentEp;
+  if (currentStats.currentPp !== currentPp) dataToUpdate.currentPp = currentPp;
+  if (currentStats.currentMp !== currentMp) dataToUpdate.currentMp = currentMp;
   if (currentStats.currentLp !== currentLp) dataToUpdate.currentLp = currentLp;
   if (currentStats.atkRollResult !== atkRollResult) dataToUpdate.atkRollResult = atkRollResult;
   if (currentStats.skillCheckResult !== skillCheckResult) dataToUpdate.skillCheckResult = skillCheckResult;
@@ -42,13 +38,23 @@ export default async function updateCharStats(req, res) {
   if (currentStats.gameId !== gameId) dataToUpdate.gameId = gameId;
   if (currentStats.initiativeWithRoll !== initiativeWithRoll) dataToUpdate.initiativeWithRoll = initiativeWithRoll;
 
-  if (Object.keys(dataToUpdate).length > 0) {
+let dataToUpdateKeys = Object.keys(dataToUpdate)
+
+let dataToUpdateForSelect = {}
+
+  for (let i = 0; i < dataToUpdateKeys.length; i++) {
+    if (dataToUpdate[dataToUpdateKeys[i]]) {
+      dataToUpdateForSelect[dataToUpdateKeys[i]] = true;
+    }
+  }
+  if (!Object.keys(dataToUpdateForSelect).length) {
+    res.status(200).json({ message: "No changes to update" });
+  } else if (dataToUpdateKeys.length > 0) {
     const updatedCharacterStats = await prisma.characterStatsThatChange.update({
       where: { charName },
+      select: dataToUpdateForSelect,
       data: dataToUpdate,
     });
     res.json(updatedCharacterStats);
-  } else {
-    res.status(200).json({ message: "No changes to update" });
   }
 }
