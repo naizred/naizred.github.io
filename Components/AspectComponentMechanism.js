9e5acc8229f6f4d5a2fcb31e7a2aa3a5522693ea
naizred <elek.balazs86@gmail.com>
2024-12-02 14:11:00 +0100

    a 12-es mechanizmus aspektus szam valamiert hianyzott

--- Components/AspectComponentMechanism.js
+++ Components/AspectComponentMechanism.js
@@ -1,5 +1,5 @@
 function AspectComponentMechanism() {
-    let mechanismAspectNumbers = [1,2,3,4,5,6,7,8,9,10,11,]
+    let mechanismAspectNumbers = [1,2,3,4,5,6,7,8,9,10,11,12]
 
     return (<>
            <li>
